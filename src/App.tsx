17c99c8303f2f6e9bb722298dc2fc71504a57a92
박윤환 <park-yunhwan@bag-yunhwan-ui-MacBookPro.local>
2024-09-13 13:52:24 +0900

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element : <Layout />,
+    children : [
+      {
+        path :"",
+        element : <Home />,
+      },
+      {
+        path : "profile",
+        element : <Profile />,
+      },
+    ] 
+  },
+  {
+    path : "/login",
+    element : <Login />
+  },
+  {
+    path : "/create-account", 
+    element : <CreateAccount />
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${onreset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', 
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 function App() {
-  return <></>;
+  return <>
+    <GlobalStyles />
+    <RouterProvider router={router} />
+  </>;
 }
 
 export default App
