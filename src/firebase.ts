d8206b43074d82db717009a4bde87c8982eb636e
박윤환 <gkdld1004@gmail.com>
2024-09-23 18:16:26 +0900

    Post Tweet Form/Tweeting to Firestore/Uploading Images

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDh02vzuo4J_agnPWC1UqzhqagYiVLxzZ4",
@@ -14,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
